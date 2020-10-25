2142d8033146b52e1e785d43c63745c94f6d1e19
RandyLu0 <lurandy8@gmail.com>
2020-10-25 18:53:34 -0400

    added files

--- Work03.c
+++ Work03.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
 int main(){
-    int a;
-    //Problem 1 
-    int sum = 0;
-    for(a = 0; a < 1001; a++){
-        if(a % 3 == 0 || a % 5 == 0){
-            sum += a;
-        }
-    }
-
-    printf("%d\n",sum);
-
-    //Problem 6
-    int squares = 0;
-    int squared = 0;
-
-    for(a = 0; a < 101; a++){
-        squared += a;
-        squares += a * a;
-    }
-    squared *= squared;
-
-    //printf("%d\n",squared - squares);
-    
+    unsigned int e = 4000000000;
+    unsigned int d = 3000000000;
+    char q = 120;
+    long long  p =  500000000000000;
+    //Fills in zeroes for unused decimal places 
+    double b = 5.268;
+    char t = 256;
+    char y = -256;
+    printf("e = %d d = %d q = %c p = %lld b = %lf t = %c y = %c\n", e,d,q,p,b,t,y);
     return 0;
 }
